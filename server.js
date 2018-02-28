9285602cd80bb0ae2d929b1690ea831bf3c65df3
Patrick Pahl <>
2018-02-27 22:53:37 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,77 @@
+// server.js
+// Where we'll configure routes and pages
+
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+// Middleware lets you configure how your express app works- Here we teach express how to read from static dir
+app.use(express.static(__dirname + '/public'));  // __dirname stores the path to the directory
+
+app.use((req, res, next) => {   // Next: you tell express when your Middleware func is done, need to call next()
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+  next();
+});
+
+
+// Route handlers:
+// '/' -> url, the route of the app so we can just use '/'
+// req -> request stores info about what is coming in, i.e. headers, body, method
+// res -> customize what date you send back
+// app.get('/', (req, res) => {
+// //res.send('<h1>Hello Express!</h1>')   // res for the http request, getting this back as the body data
+// res.send({
+//   name: 'Puff',
+//   likes: [
+//     'Tennis',
+//     'Bowling',
+//     'IPAs'
+//   ]
+// })
+// });
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home page',
+    currentYear: new Date().getFullYear(),
+    welcomeMessage: 'Welcome to my dope azz website on local host 3000!'
+  });
+});
+
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
+});
+//*View about by going to localhost:3000/about
+
+app.get('/bad', (req, res) =>   {
+  res.send({
+    errorMessage: 'Unable to process the request'
+  });
+});
+
+
+//app.listen binds the application to a port on our machine
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
+
+//*Terminal: 'node server.js' to start listening
+// THEN ctrl + c to stop listening
+//*Browser: localhost:3000    THEN you can see Hello Express print out in browser
+// option/command/i -> Developer tools on chrome
